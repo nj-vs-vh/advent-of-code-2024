94a3a6a6bef9f3757c8cc25985eba9c2e55b24db
Igor Vaiman <igor.vaiman@inten.to>
2024-12-21 19:24:23 +0100

    d20 unified solution

--- day20/solution.py
+++ day20/solution.py
@@ -31,37 +31,27 @@ def build_path_map(inp: str) -> Map2D[int | None]:
     return path_map
 
 
-def part_1(inp: str, debug: bool):
-    path_map = build_path_map(inp)
-
+def count_skips_above_100(path_map: Map2D[int | None], skip_len: int) -> int:
     skips: list[tuple[tuple[int, int], tuple[int, int], int]] = []
     for i, j, d_before in path_map.iter_cells():
         skip_start = (i, j)
         if d_before is None:
             continue
-        for skip_end, delta in manhattan_neighborhood(r=2, center=skip_start):
+        for skip_end, delta in manhattan_neighborhood(r=skip_len, center=skip_start):
             d_after = path_map.at(*skip_end)
             if d_after is None or d_after < d_before:
                 continue
             skip = (d_after - d_before) - delta
             if skip > 99:
                 skips.append((skip_start, skip_end, skip))
-    print(len(skips))
+    return len(skips)
+
+
+def part_1(inp: str, debug: bool):
+    path_map = build_path_map(inp)
+    print(count_skips_above_100(path_map, skip_len=2))
 
 
 def part_2(inp: str, debug: bool):
     path_map = build_path_map(inp)
-
-    skips: list[tuple[tuple[int, int], tuple[int, int], int]] = []
-    for i, j, d_before in path_map.iter_cells():
-        skip_start = (i, j)
-        if d_before is None:
-            continue
-        for skip_end, delta in manhattan_neighborhood(r=20, center=skip_start):
-            d_after = path_map.at(*skip_end)
-            if d_after is None or d_after < d_before:
-                continue
-            skip = (d_after - d_before) - delta
-            if skip > 99:
-                skips.append((skip_start, skip_end, skip))
-    print(len(skips))
+    print(count_skips_above_100(path_map, skip_len=20))
